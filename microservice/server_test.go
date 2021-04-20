2d4b1fdca5cd9da61e79b9340a8c74a2faf2ba9b
will7007 <38322012+will7007@users.noreply.github.com>
2021-04-20 17:37:53 -0400

    Update server_test.go

--- microservice/server_test.go
+++ microservice/server_test.go
@@ -15,7 +15,7 @@ var (
 // Unit Tests
 
 func TestHandler(t *testing.T) {
-	expected := []byte("Hello World!\n")
+	expected := []byte("Did it actually update this time?\n")
 
 	req, err := http.NewRequest("GET", buildUrl("/"), nil)
 	if err != nil {
